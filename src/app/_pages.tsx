0868c90bea81fdb11c57a1be7ea5f1d3266c4ceb
Sruthi Sanala <dineshsanala4@gmail.com>
2023-11-16 10:22:58 +0530

    initial app setup

---
+++ src/app/_pages.tsx
@@ -0,0 +1,12 @@
+import "./styles/globals.css";
+import Layout from "./components/layout/Layout";
+
+const Home = () => {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+};
+
+export default Home;
