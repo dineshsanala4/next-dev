2f5cf6f791279da9bc0c60de333311e953f92dd2
Sruthi Sanala <dineshsanala4@gmail.com>
2023-11-24 09:47:06 +0530

    home page bug fixes

--- src/app/components/meetups/MeetupList.tsx
+++ src/app/components/meetups/MeetupList.tsx
@@ -1,7 +1,7 @@
 import MeetupItem from './MeetupItem';
 import classes from './MeetupList.module.css';
 
-function MeetupList(props) {
+function MeetupList(props:any) {
   return (
     <ul className={classes.list}>
       {props.meetups.map((meetup:any) => (
